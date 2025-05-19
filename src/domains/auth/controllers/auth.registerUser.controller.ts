6bca7ec0f8277346ce19dc6d51f5de60f2c2f8b9
Okpainmo <okpainmoandrew@gmail.com>
2025-05-19 15:43:44 +0100

    chore: auth domain updates

--- src/domains/auth/controllers/auth.registerUser.controller.ts
+++ src/domains/auth/controllers/auth.registerUser.controller.ts
@@ -13,9 +13,20 @@ import { createUser__postgres } from '../lib/postgres__auth.createUser.service.j
 // import { findUser__mongo } from '../../user/lib/mongo__user.findUser.service.js';
 import { findUser__postgres } from '../../user/lib/postgres__user.findUser.service.js';
 import { errorHandler__500 } from '../../../utils/errorHandlers/codedErrorHandlers.js';
+import { hashingHandler } from '../../../utils/hashingHandler.js';
+import { deployAuthCookie } from '../../../utils/cookieDeployHandlers.js';
+import { generateTokens } from '../../../utils/generateTokens.js';
+// import { updateUser__mongo } from '../../user/lib/mongo__user.updateUser.service.js';
+import { updateUser__postgres } from '../../user/lib/postgres__user.updateUser.service.js';
 
-// type UserProfileResponse = Pick<UserSpecs, '_id' | 'name' | 'email' | 'isAdmin' | 'isActive' | 'createdAt' | 'updatedAt'>;
-type UserProfileResponse = Pick<UserSpecs, 'id' | 'name' | 'email' | 'isAdmin' | 'isActive' | 'createdAt' | 'updatedAt'>;
+// type UserProfileResponse = Pick<
+//   UserSpecs,
+//   '_id' | 'name' | 'email' | 'isAdmin' | 'isActive' | 'createdAt' | 'updatedAt' | 'accessToken' | 'refreshToken'
+// >;
+type UserProfileResponse = Pick<
+  UserSpecs,
+  'id' | 'name' | 'email' | 'isAdmin' | 'isActive' | 'createdAt' | 'updatedAt' | 'accessToken' | 'refreshToken'
+>;
 
 type inSpecs = {
   name: string;
@@ -31,10 +42,18 @@ type ResponseSpecs = {
   };
 };
 
+type AuthTokenSpecs = {
+  authCookie: string;
+  accessToken: string;
+  refreshToken: string;
+};
+
 export const registerUser = async (req: Request<{}, ResponseSpecs, inSpecs>, res: Response<ResponseSpecs>) => {
   try {
     // const existingUser = await findUser__mongo({ email: req.body.email });
     const existingUser = await findUser__postgres({ email: req.body.email });
+
+    const hashedPassword = await hashingHandler({ stringToHash: req.body.password });
 
     if (existingUser) {
       res.status(400).json({
@@ -43,26 +62,56 @@ export const registerUser = async (req: Request<{}, ResponseSpecs, inSpecs>, res
       });
     }
 
+    req.body.password = hashedPassword;
     const registeredUser = await createUser__postgres({ user: req.body });
     // const registeredUser = await createUser__mongo({ user: req.body });
-    // log.info(registeredUser);
 
-    if (registeredUser) {
-      res.status(201).json({
-        responseMessage: 'User registered successfully',
-        response: {
-          userProfile: {
-            // _id: registeredUser._id, # mongo
-            id: registeredUser.id,
-            name: registeredUser.name,
-            email: registeredUser.email,
-            isAdmin: registeredUser.isAdmin,
-            isActive: registeredUser.isActive,
-            createdAt: registeredUser.createdAt,
-            updatedAt: registeredUser.updatedAt
+    // if (registeredUser && registeredUser.email && registeredUser._id) {
+    if (registeredUser && registeredUser.email && registeredUser.id) {
+      // generate auth tokens
+      const authTokens = await generateTokens({ tokenType: 'auth', user: { id: registeredUser.id, email: registeredUser.email } });
+      // const authTokens = await generateTokens({ tokenType: 'auth', user: { id: registeredUser._id, email: registeredUser.email } });
+
+      // authTokenSpecs from global.d.ts
+      const { accessToken, refreshToken, authCookie } = authTokens as AuthTokenSpecs;
+
+      if (accessToken && refreshToken && authCookie) {
+        // await updateUser__mongo({
+        //   userId: registeredUser._id,
+        //   // userId: registeredUser.id,
+        //   email: registeredUser.email,
+        //   requestBody: { accessToken: accessToken, refreshToken: refreshToken }
+        // });
+
+        await updateUser__postgres({
+          userId: registeredUser.id,
+          email: registeredUser.email,
+          requestBody: {
+            accessToken: accessToken,
+            refreshToken: refreshToken
           }
-        }
-      });
+        });
+
+        deployAuthCookie({ authCookie: authCookie }, res);
+
+        res.status(201).json({
+          responseMessage: 'User registered successfully',
+          response: {
+            userProfile: {
+              // _id: registeredUser._id,
+              id: registeredUser.id,
+              name: registeredUser.name,
+              email: registeredUser.email,
+              isAdmin: registeredUser.isAdmin,
+              isActive: registeredUser.isActive,
+              createdAt: registeredUser.createdAt,
+              updatedAt: registeredUser.updatedAt,
+              accessToken: accessToken,
+              refreshToken: refreshToken
+            }
+          }
+        });
+      }
     }
   } catch (error) {
     errorHandler__500(error, res);
--- src/domains/auth/lib/postgres__auth.createUser.service.ts
+++ src/domains/auth/lib/postgres__auth.createUser.service.ts
@@ -1,13 +1,17 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../../../generated/prisma/index.js';
 import type { UserSpecs } from '../../user/schema/user.schema.js';
 import { customServiceErrorHandler } from '../../../utils/errorHandlers/customServiceErrorHandler.js';
+// import { userModel } from '../../user/models/user.model.js';
+// import log from '../../../utils/logger.js';
 
 const prisma = new PrismaClient();
 
 export async function createUser__postgres(data: { user: UserSpecs }) {
   try {
     // Create new user
-    if (data.user.email && data.user.password && data.user.password) {
+    if (data.user.email && data.user.name && data.user.password) {
+      // log.info(`${data.user.email}, ${data.user.password}, ${data.user.name}`);
+
       const user = await prisma.user.create({
         data: {
           name: data.user.name,
@@ -35,3 +39,33 @@ export async function createUser__postgres(data: { user: UserSpecs }) {
     return;
   }
 }
+
+// export async function createUser__mongo(data: { user: UserSpecs }) {
+//   try {
+//     const { name, email, password } = data.user;
+
+//     // Create user
+//     const newUser: UserSpecs = await userModel.create({
+//       name,
+//       email,
+//       password
+//     });
+
+//     // Selectively return fields (excluding password)
+//     const user = {
+//       _id: newUser._id,
+//       name: newUser.name,
+//       email: newUser.email,
+//       isAdmin: newUser.isAdmin,
+//       isActive: newUser.isActive,
+//       createdAt: newUser.createdAt,
+//       updatedAt: newUser.updatedAt
+//     };
+
+//     return user;
+//   } catch (error) {
+//     customServiceErrorHandler(error);
+
+//     return;
+//   }
+// }

70f401369ed67c6472ef40ebbda564544b146682
Okpainmo <okpainmoandrew@gmail.com>
2025-05-19 15:46:51 +0100

    chore: global ts updates

--- globals.d.ts
+++ globals.d.ts
@@ -1,1 +1,23 @@
 // ts globals
+import type { UserSpecs } from './src/domains/user/schema/user.schema.ts';
+
+// Augment the Express Request type - more like extending express(with ts) globally
+declare module 'express' {
+  interface Request {
+    userData?: {
+      user: UserSpecs;
+      sessionStatus?: string;
+      // renewedUserAccessToken?: string;
+      // renewedUserRefreshToken?: string;
+      newUserAccessToken?: string;
+      newUserRefreshToken?: string;
+    };
+  }
+
+  // interface Response {
+  //   response?: {
+  //     accessToken: string;
+  //     tokenStatus: string;
+  //   };
+  // }
+}
